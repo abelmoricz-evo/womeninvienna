97732709abe2c029878c7a1f04472fbfa8e9d91f
abelmoricz-evo <102213273+abelmoricz-evo@users.noreply.github.com>
2022-12-31 14:20:30 +0100

    Update main.js

--- static/main.js
+++ static/main.js
@@ -19,7 +19,7 @@ var graphData = {
 var myChart = new Chart(ctx, graphData);
 
 //var socket = new WebSocket('ws://localhost:8000/ws/graph/');
-var socket = new WebSocket('ws://womeninvienna.com/ws/graph/');
+var socket = new WebSocket('https://womeninvienna.com/ws/graph/');
 
 socket.onmessage = function(e){
     var djangoData = JSON.parse(e.data);
